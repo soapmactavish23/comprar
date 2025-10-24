22b34c19b078b8620a02e2b6e2de7652afd03a9b
henrick.nogueira <ricknogueira1231@gmail.com>
2025-10-24 19:49:51 -0300

    first commit

---
+++ src/app/home/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { styles } from "./styles";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Oi Aninha</Text>
+    </View>
+  );
+}
