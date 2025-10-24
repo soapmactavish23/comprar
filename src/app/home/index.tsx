748c9f5446d9231d37e0b84f46c1c171c037afad
henrick.nogueira <ricknogueira1231@gmail.com>
2025-10-24 20:01:22 -0300

    Componente Button

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,10 +1,12 @@
 import { Image, Text, View } from "react-native";
 import { styles } from "./styles";
+import { Button } from "@/components/Button";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Image style={styles.logo} source={require("@/assets/logo.png")} />
+      <Button />
     </View>
   );
 }
