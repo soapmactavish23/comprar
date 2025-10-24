76ed01ef982395844da99549bbc26708bfa84184
henrick.nogueira <ricknogueira1231@gmail.com>
2025-10-24 20:12:11 -0300

    Componente Input

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,14 +1,13 @@
 import { Image, Text, View } from "react-native";
 import { styles } from "./styles";
 import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Image style={styles.logo} source={require("@/assets/logo.png")} />
-      <Button title="Adicionar" />
-      <Button title="Adicionar" />
-      <Button title="Adicionar" />
+      <Input placeholder="O que você precisa comprar?" />
       <Button title="Adicionar" />
     </View>
   );
